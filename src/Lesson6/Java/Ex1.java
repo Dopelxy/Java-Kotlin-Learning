4850b1690bacb9c42019a73a8b45942909c3538c
fanat <fanat.naim@gmail.com>
2026-05-13 16:57:34 +0300

    Ex1 java and kotlin

--- src/Lesson6/Java/Ex1.java
+++ src/Lesson6/Java/Ex1.java
@@ -1,4 +1,38 @@
 package Lesson6.Java;
 
+import java.util.Arrays;
+
 public class Ex1 {
+
+    public static void main(String[] args) {
+
+        // Original array
+        int[] arr = {4, -3, 2, -1, 0, -5};
+        // New array
+        int[] result = new int[arr.length];
+
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] < 0) {
+
+                result[index] = arr[i];
+
+                index++;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] >= 0) {
+
+                result[index] = arr[i];
+
+                index++;
+            }
+        }
+        // Display result
+        System.out.println(Arrays.toString(result));
+    }
 }
--- src/Lesson6/forkotlin/Ex1.kt
+++ src/Lesson6/forkotlin/Ex1.kt
@@ -1,2 +1,29 @@
 package Lesson6.forkotlin
 
+fun main(args: Array<String>) {
+    //Original array
+    val arr = intArrayOf(4, -3, 2, -1, 0, -5)
+    // New array
+    val result = IntArray(arr.size)
+
+    var index = 0
+
+    for (i in arr.indices) {
+        if (arr[i] < 0){
+            result [index] = arr[i]
+            index++
+
+        }
+    }
+
+    for (i in arr.indices) {
+        if (arr[i] >= 0){
+            result [index] = arr[i]
+            index++
+        }
+    }
+    println("Original array: ${arr.joinToString()}")
+    println("New array: ${result.joinToString ()}")
+
+
+}
